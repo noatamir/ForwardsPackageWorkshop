a77ea3f820394900240cd0156170272522ea601f
Noa Tamir <hello@noatamir.com>
2018-05-14 13:59:01 +0200

    roxy documentation 1

--- R/add_cols_function.R
+++ R/add_cols_function.R
@@ -8,3 +8,7 @@ add_cols <- function(x, y, where = 1) {
     cbind(x[lhs], y, x[-lhs])
   }
 }
+ #' @param x data frame to insert columns into
+ #' @param y the columns you want to insert
+ #' @param where ordinal location for the insert, 1 means before all of the columns in x
+ 
