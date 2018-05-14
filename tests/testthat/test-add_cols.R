ed1bf692a19fff1d850e82731826e78c74e2a055
Noa Tamir <hello@noatamir.com>
2018-05-14 12:42:27 +0200

    clean up test

--- tests/testthat/test-add_cols.R
+++ tests/testthat/test-add_cols.R
@@ -1,4 +1,10 @@
-test_that("can add column at any position", { at_pos <- function(i) {
-  add_cols(df1, df2, where = i) }
-expect_named(at_pos(1), c("X", "Y", "a", "b", "c")) expect_named(at_pos(2), c("a", "X", "Y", "b", "c")) expect_named(at_pos(3), c("a", "b", "X", "Y", "c")) expect_named(at_pos(4), c("a", "b", "c", "X", "Y"))
+test_that("can add column at any position", { 
+  at_pos <- function(i) {
+    add_cols(df1, df2, where = i) 
+  }
+  
+  expect_named(at_pos(1), c("X", "Y", "a", "b", "c")) 
+  expect_named(at_pos(2), c("a", "X", "Y", "b", "c")) 
+  expect_named(at_pos(3), c("a", "b", "X", "Y", "c")) 
+  expect_named(at_pos(4), c("a", "b", "c", "X", "Y"))
 })
